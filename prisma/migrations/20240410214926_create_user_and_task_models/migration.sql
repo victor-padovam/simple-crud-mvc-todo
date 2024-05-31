216982a0622f4ba0fdc9a25f82864a609519d02a
Victor Padovam <43040419+victor-padovam@users.noreply.github.com>
2024-05-31 16:57:20 -0300

    up

--- prisma/migrations/20240410214926_create_user_and_task_models/migration.sql
+++ prisma/migrations/20240410214926_create_user_and_task_models/migration.sql
@@ -1,18 +1,18 @@
 -- CreateTable
-CREATE TABLE "User" (
+CREATE TABLE "user" (
     "id" INTEGER NOT NULL PRIMARY KEY,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL
 );
 
 -- CreateTable
-CREATE TABLE "Task" (
+CREATE TABLE "task" (
     "id" INTEGER NOT NULL PRIMARY KEY,
     "title" TEXT NOT NULL,
     "completed" BOOLEAN NOT NULL,
     "userId" INTEGER NOT NULL,
-    CONSTRAINT "Task_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+    CONSTRAINT "Task_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+CREATE UNIQUE INDEX "user_email_key" ON "user"("email");

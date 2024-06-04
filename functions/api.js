8a596924b4318e5e7fdf08edfc4354d35d9bae3f
Victor Padovam <43040419+victor-padovam@users.noreply.github.com>
2024-06-04 15:30:33 -0300

    up

--- functions/api.js
+++ functions/api.js
@@ -3,15 +3,17 @@ const serverless = require('serverless-http');
 const app = express();
 const router = express.Router();
 
-app.use(json());
 app.use(express.json());
 
-app.get('/tasks', getAllTasks);
-app.post('/tasks', createTask);
-app.put('/tasks/:id', updateTask);
-app.delete('/tasks/:id', deleteTask);
-app.post('/register', register);
-app.post('/login', login);
+// Define your routes
+router.get('/tasks', getAllTasks);
+router.post('/tasks', createTask);
+router.put('/tasks/:id', updateTask);
+router.delete('/tasks/:id', deleteTask);
+router.post('/register', register);
+router.post('/login', login);
 
+// Use the router
 app.use('/.netlify/functions/api', router);
+
 module.exports.handler = serverless(app);

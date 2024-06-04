8e44baf6ccc03d92941f6cf1bbf422f40f9f7b6b
Victor Padovam <43040419+victor-padovam@users.noreply.github.com>
2024-06-04 15:02:43 -0300

    up

--- index.js
+++
@@ -1,6 +0,0 @@
-const { app } = require('./src/routes.js');
-
-// Esta é a função que o Vercel chamará para lidar com as solicitações recebidas
-module.exports = (req, res) => {
-    app(req, res);
-};

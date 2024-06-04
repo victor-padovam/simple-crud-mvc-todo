2690fe3f588960a68e1a33a78334db54bfb88455
Victor Padovam <43040419+victor-padovam@users.noreply.github.com>
2024-06-04 15:27:01 -0300

    up

--- functions/api.js
+++ functions/api.js
@@ -3,48 +3,15 @@ const serverless = require('serverless-http');
 const app = express();
 const router = express.Router();
 
-let records = [];
+app.use(json());
+app.use(express.json());
 
-//Get all students
-router.get('/', (req, res) => {
-  res.send('App is running..');
-});
-
-//Create new record
-router.post('/add', (req, res) => {
-  res.send('New record added.');
-});
-
-//delete existing record
-router.delete('/', (req, res) => {
-  res.send('Deleted existing record');
-});
-
-//updating existing record
-router.put('/', (req, res) => {
-  res.send('Updating existing record');
-});
-
-//showing demo records
-router.get('/demo', (req, res) => {
-  res.json([
-    {
-      id: '001',
-      name: 'Smith',
-      email: 'smith@gmail.com',
-    },
-    {
-      id: '002',
-      name: 'Sam',
-      email: 'sam@gmail.com',
-    },
-    {
-      id: '003',
-      name: 'lily',
-      email: 'lily@gmail.com',
-    },
-  ]);
-});
+app.get('/tasks', getAllTasks);
+app.post('/tasks', createTask);
+app.put('/tasks/:id', updateTask);
+app.delete('/tasks/:id', deleteTask);
+app.post('/register', register);
+app.post('/login', login);
 
 app.use('/.netlify/functions/api', router);
 module.exports.handler = serverless(app);
